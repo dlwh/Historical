524309f24d47605c1c08de70ffff57b773e42a86
David Hall <dlwh@stanford.edu>
2009-08-24 14:16:10 -0700

    Read in a World Atlas of Language

---
+++ src/main/scala/dlwh/historical/wals.scala
@@ -0,0 +1,43 @@
+package dlwh.historical;
+
+import scala.io._;
+import scala.collection.mutable.ArrayBuffer;
+
+object WALS {
+
+  case class Language(shortName: String, name: String, altLang: String, coords: (Double,Double), 
+    family: String, genus: String, features: Seq[Int], eths: Map[String,String]);
+
+  def load(): Seq[Language] = {
+    val stream = this.getClass.getClassLoader().getResourceAsStream("wals_new");
+    // read until we get to a blank line.
+    val lines = Source.fromInputStream(stream).getLines().dropWhile(_ != "").drop(1);
+
+    val buf = new ArrayBuffer[Language];
+    for (descripLine <- lines) {
+      val featureLine = lines.next.trim;
+      val Array(_, _shortName, _name, _altLang, "?", coordString, _family, _genus) = descripLine.split("\t");
+      val _coords = {
+        val Array(lat,long) = coordString.split(" ").map(_.toDouble);
+        (lat,long);
+      }
+      val _features = featureLine.split(" ").map { case "?" => -1 case x => x.toInt};
+      
+      var ethLines = lines.takeWhile(_ != "");
+      val _eths = Map.empty ++ ( for(e <- ethLines) yield {
+        val Array("eth",key,value) = e.trim.split("\t");
+        (key,value);
+      } );
+
+      buf += Language ( shortName = _shortName, name = _name, 
+        altLang = _altLang, coords = _coords,
+        family = _family, genus = _genus,
+        features = _features, eths = _eths
+      )
+    }
+
+    stream.close();
+    buf
+  }
+
+}
