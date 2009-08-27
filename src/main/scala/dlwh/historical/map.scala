07300d335dddea907528434834e21c681f1c4b17
David Hall <dlwh@berkeley.edu>
2009-08-26 19:50:39 -0700

    Plot circles.

--- src/main/scala/dlwh/historical/map.scala
+++ src/main/scala/dlwh/historical/map.scala
@@ -9,4 +9,18 @@ object Plot {
     val longs = langs.map(_.coords._2).toArray;
     scatter(longs,lats,ones(longs.length),ones(longs.length));
   }
+
+  def circle(center: (Double,Double), radius: Double) {
+    val t = linspace(0,2*Math.Pi);
+    val x = t.like;
+    x := cos(t);
+    x *= radius;
+    x += center._1;
+
+    val y = t.like;
+    y := sin(t);
+    y *= radius;
+    y += center._2;
+    plot(x,y);
+  }
 }
