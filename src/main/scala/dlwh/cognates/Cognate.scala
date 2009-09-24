553db4e03b5b2594798c19fdea7183613784e9eb
David Hall <dlwh@berkeley.edu>
2009-09-24 11:23:30 -0700

    Clean up of cognate and dealing with new Arrays (ugh)

--- src/main/scala/dlwh/cognates/Cognate.scala
+++ src/main/scala/dlwh/cognates/Cognate.scala
@@ -1,14 +1,14 @@
 package dlwh.cognates;
 import scala.io._;
 
-case class Cognate(word: String, trueLanguage: String);
+case class Cognate(word: String, language: String);
 
 object Romance {
   def readCognates(): Seq[Seq[Cognate]]= {
     val stream = this.getClass.getClassLoader().getResourceAsStream("romance_ipa");
     val src = Source.fromInputStream(stream)(Codec.UTF8).getLines().drop(1);
     val cognates = (for(line <- src) yield {
-      for( (w,l) <- line.split(' ') zip Array("Spanish","Italian","Latin","Portuguese")) 
+      for( ((w,c),l) <- line.split(' ').zipWithIndex.toSequence zip Seq("Spanish","Italian","Latin","Portuguese")) 
         yield Cognate(w,l);
     }).toSequence;
 
