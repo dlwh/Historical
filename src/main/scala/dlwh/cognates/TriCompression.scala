0b6e0d72460b0bf299529cda5e054cfc7b5100ee
David Hall <dlwh@berkeley.edu>
2009-12-07 16:42:47 -0800

    Trigram compression supports # of states.
    Not great yet.

--- src/main/scala/dlwh/cognates/TriCompression.scala
+++ src/main/scala/dlwh/cognates/TriCompression.scala
@@ -1,13 +1,20 @@
 package dlwh.cognates;
 
+import scala.collection.mutable.ArrayBuffer;
+
 import scalanlp.fst._;
 import scalanlp.util.Index;
 import scalanlp.counters.LogCounters._;
 import scalanlp.math.Numerics._;
+import scala.collection.mutable.PriorityQueue;
+import scalanlp.math.Semiring.LogSpace._;
+import Automaton._;
 
-class TriCompression(klThreshold: Double) {
+
+class TriCompression(klThreshold: Double, maxStates: Int) {
   import TrigramSemiring._;
   import ring._;
+  require(maxStates >= 1);
   def handleAuto(auto: Automaton[Double,Int,Char]) = {
     auto.reweight(promote[Int] _ , promoteOnlyWeight _).cost.counts;
   }
@@ -22,6 +29,52 @@ class TriCompression(klThreshold: Double) {
     result;
   }
 
+  def selectStates(maxStates:Int,
+                   counts: LogPairedDoubleCounter[Gram,Char],
+                   comparisonGram: Gram=>LogDoubleCounter[Char]) = {
+    def orderGrams(g1: (Gram,LogDoubleCounter[Char],Double),
+                   g2: (Gram,LogDoubleCounter[Char],Double)) = (
+      g1._3 < g2._3
+    )
+    implicit val tupleOrdering = Ordering.fromLessThan(orderGrams _ )
+
+    val pq = new PriorityQueue[(Gram,LogDoubleCounter[Char],Double)]();
+
+    for {
+      (gram:Unigram,ctr) <- counts.rows;
+      comparison = comparisonGram(gram);
+      kl = klDivergence(ctr,comparison)
+  //    () = println(kl);
+      if kl > klThreshold
+    } {
+      pq += ((gram,ctr,kl));
+    }
+
+    val result = new ArrayBuffer[(Gram,LogDoubleCounter[Char])];
+    while(result.size < maxStates-1 && !pq.isEmpty) {
+      val (gram,ctr,_) = pq.dequeue();
+
+      result += ((gram,ctr));
+
+      gram match {
+        case Unigram(ch) =>
+        // TODO: make this more efficient by precomputing successors
+        for {
+          (gram2 @ Bigram(`ch`,ch2),ctr) <- counts.rows;
+          comparison = comparisonGram(gram);
+          kl = klDivergence(ctr,comparison)
+          //() = println(kl);
+          if kl > klThreshold
+        } {
+          pq += ((gram2,ctr,kl));
+        }
+        case _ => () // do nothing
+      }
+    }
+
+    Map.empty ++ result
+  }
+
   def compress(ao: Automaton[Double,Int,Char]) = {
     val counts : LogPairedDoubleCounter[Gram,Char] = handleAuto(ao);
     val marginal = marginalizeCounts(counts);
@@ -33,33 +86,29 @@ class TriCompression(klThreshold: Double) {
       case _ => error("Unexpected" + gram);
     }
 
-    val gramIndex = Index[Gram]();
-    val divergentStates = Map.empty ++ (for {
-      (gram,ctr) <- counts.rows;
-      if gram != Noncegram
-      comparison = comparisonGram(gram);
-      kl = klDivergence(ctr,comparison)
-      () = println(kl);
-      if kl > klThreshold
-    } yield (gram,ctr));
+    val selectedStates = selectStates(maxStates,counts, comparisonGram _);
 
-    import scalanlp.math.Semiring.LogSpace._;
-    import Automaton._;
+    val gramIndex = Index[Gram]();
+
+    // a few useful states
+
+    val endState = -2;
+    val unigramState = -1
+    // Compute the start state last
     val startState = {
-      if(divergentStates contains Unigram('#'))
+      if(selectedStates contains Unigram('#'))
         gramIndex(Unigram('#'))
-      else if(divergentStates contains Bigram('#','#'))
+      else if(selectedStates contains Bigram('#','#'))
         gramIndex(Bigram('#','#'))
       else -1;
     }
-    val endState = -2;
-    val unigramState = -1
+
 
     // compute destination for an arc 
     def destinationFor(gram: Gram, ch2: Char) = {
       if(ch2 == '#') -2
       else if(gram == Noncegram) {
-        if(divergentStates contains Unigram(ch2)) gramIndex(Unigram(ch2));
+        if(selectedStates contains Unigram(ch2)) gramIndex(Unigram(ch2));
         else unigramState
       } else {
         val bg = gram match {
@@ -67,8 +116,8 @@ class TriCompression(klThreshold: Double) {
           case u: Unigram => Bigram(u._1,ch2);
           case _ => error("Shouldn't be here"); 
         }
-        if(divergentStates contains bg) gramIndex(bg);
-        else if(divergentStates contains Unigram(ch2)) gramIndex(Unigram(ch2));
+        if(selectedStates contains bg) gramIndex(bg);
+        else if(selectedStates contains Unigram(ch2)) gramIndex(Unigram(ch2));
         else unigramState
       }
     }
@@ -79,18 +128,20 @@ class TriCompression(klThreshold: Double) {
       chreal = if(ch2 == '#') ao.inAlpha.epsilon else ch2
     } yield Arc(unigramState,idx2,chreal,chreal,w-marginal.logTotal);
 
+    // mainloop: until we have enough states, add arcs for that state, also enqueue
+    // successor states (bigrams)
+    var numStates = 1;
     val divArcs = for {
-      (gram,ctr) <- divergentStates.iterator;
+      (gram,ctr) <- selectedStates.iterator;
       idx1 = gramIndex(gram);
-      (ch2,w) <- ctr.iterator;
-      idx2 = destinationFor(gram,ch2)
+      (ch2,w) <- marginal.iterator;
+      idx2 = destinationFor(gram,ch2);
       chreal = if(ch2 == '#') ao.inAlpha.epsilon else ch2
-     } yield Arc(idx1,idx2,chreal,chreal,w-ctr.logTotal);
+    } yield Arc(idx1,idx2,chreal,chreal,w-marginal.logTotal);
 
     val auto = automaton(Map(startState->0.0),Map(endState->0.0))(
       (unigramArcs ++ divArcs).toSeq :_*
     );
     auto
-    
   }
 }
