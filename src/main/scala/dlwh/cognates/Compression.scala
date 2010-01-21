b1decaf22a38c21cfbe6e99a57ef262efc79744b
David Hall <dlwh@berkeley.edu>
2010-01-20 16:13:20 -0800

    BiCompression now looks like TriCompression

--- src/main/scala/dlwh/cognates/Compression.scala
+++ src/main/scala/dlwh/cognates/Compression.scala
@@ -3,73 +3,153 @@ package dlwh.cognates;
 import scalanlp.fst._;
 import scalanlp.util.Index;
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.PriorityQueue
 import scalanlp.counters.LogCounters._;
 import scalanlp.math.Numerics._;
-
-class Compression(klThreshold: Double) {
-  import BigramSemiring._;
-  import ring._;
-  def handleAuto(auto: Automaton[Double,Int,Char]) = {
-    auto.reweight(promote[Int] _ , promoteOnlyWeight _).cost.counts;
-  }
-  def compress(ao: Automaton[Double,Int,Char]) = {
-    val counts : LogPairedDoubleCounter[Char,Char] = handleAuto(ao);
-    val marginal = marginalize(counts);
-    // special handling for # in the marginal case, since it represents the start token
-    // but we want the end token.
-    val endCount = logSum((for {
-      (_,ctr) <- counts.rows;
-      v = ctr('#')
-    } yield v).toSeq);
-    
-    marginal('#') = endCount;
-
-    val charIndex = Index[Char]();
-    val divergentStates = Map.empty ++ (for {
-      (ch,ctr) <- counts.rows;
-      kl = klDivergence(ctr,marginal)
-      () = println(kl);
-      if kl > klThreshold
-    } yield (ch,ctr));
-
-    import scalanlp.math.Semiring.LogSpace._;
-    import Automaton._;
-    val startState = if(divergentStates contains '#') charIndex('#') else -1;
-    val endState = -2;
-
-    val unigramArcs = for {
-      (ch2,w) <- marginal.iterator;
-      idx2 = if(ch2 == '#') -2
-        else if(divergentStates contains ch2) charIndex(ch2)
-        else -1
-      chreal = if(ch2 == '#') implicitly[Alphabet[Char]].epsilon else ch2
-     } yield Arc(-1,idx2,chreal,w-marginal.logTotal);
-
-
-    val divArcs = for {
-      (ch1,ctr) <- divergentStates.iterator;
-      idx1 = charIndex(ch1);
-      (ch2,w) <- ctr.iterator;
-      idx2 = if(ch2 == '#') -2
-        else if(divergentStates contains ch2) charIndex(ch2)
-        else -1
-      chreal = if(ch2 == '#') implicitly[Alphabet[Char]].epsilon else ch2
-     } yield Arc(idx1,idx2,chreal,w-ctr.logTotal);
-
-    val auto = automaton(Map(startState->0.0),Map(endState->0.0))(
-      (unigramArcs ++ divArcs).toSeq :_*
-    );
-    auto
-
-
-    
-  }
-}
 
 trait Compressor[State,T] {
   def destinationFor(state: State, trans: T): State
   def beginningUnigram: T
   protected def alphabet: Alphabet[T];
+}
+
+abstract class BiCompression[@specialized("Char") T:Alphabet](klThreshold: Double,
+                                                      maxStates: Int,
+                                                      chars: Set[T],
+                                                      val beginningUnigram: T)
+                                                      extends Compressor[Option[T],T] with ArcCreator[Option[T],T] {
+  import BigramSemiring._;
+  require(maxStates >= 1);
+
+  protected def alphabet = implicitly[Alphabet[T]];
+  
+  private def marginalizeCounts(bigrams: LogPairedDoubleCounter[T,T]) = {
+    val marginals = LogDoubleCounter[T];
+    for( (b,ctr) <- bigrams.rows;
+      (ch,v) <- ctr) {
+      if(b != (('#','#')) || ch != (('#','#'))) {
+        marginals(ch) = logSum(marginals(ch),v);
+      }
+    }
+
+    marginals;
+  }
+  
+  private sealed abstract class State {
+    val transitions: LogDoubleCounter[T];
+    val score: Double;
+    def chars : Option[T];
+  }
+  private case class UniState(ch: T, transitions: LogDoubleCounter[T], score: Double) extends State {
+    def chars = Some(ch);
+  }
+  private case class NullState(transitions: LogDoubleCounter[T], score: Double) extends State {
+    def chars = None;
+  }
+
+  private def selectStates(maxStates:Int,
+                   marginal: LogDoubleCounter[T],
+                   bigrams: LogPairedDoubleCounter[T,T]) = {
+    def orderStates(g1: State, g2: State) = {
+      g1.score < g2.score
+    }
+    implicit val stateOrdering = Ordering.fromLessThan(orderStates _ )
+
+    val pq = new PriorityQueue[State]();
+
+    for {
+      (history,ctr) <- bigrams.rows;
+      kl = klDivergence(ctr,marginal) * Math.exp( ctr.logTotal - marginal.logTotal);
+      () = println(history + " " + kl);
+      if kl > klThreshold
+    } {
+      pq += UniState(history,ctr,kl);
+    }
+
+    val result = scala.collection.mutable.Map[Option[T],State]();
+    result += (None -> NullState(marginal,Double.PositiveInfinity));
+    while(result.size < maxStates && !pq.isEmpty) {
+      val oldState = pq.dequeue();
+
+      result += ((oldState.chars,oldState));
+    }
+
+    Map.empty ++ result;
+  }
+
+  def compress(auto: Automaton[Double,_,T]):Automaton[Double,Option[T], T] = {
+    // Set up the semiring
+    val tgs = new BigramSemiring[T](chars,beginningUnigram,cheatOnEquals=true);
+    import tgs._;
+    import ring._;
+
+    println("Enter");
+    val cost = auto.reweight(promote[Any] _, promoteOnlyWeight _ ).cost;
+    println("Exit");
+    compress(cost.totalProb,cost.counts);
+  }
+
+
+  val gramIndex = Index[Option[T]]();
+
+
+  override def destinationFor(s: Option[T], trans: T):Option[T] = {
+    var whole = Some(trans);
+    if(gramIndex.contains(whole)) {
+      whole
+    } else {
+      None;
+    }
+  }
+
+  def compress(prob: Double,
+               bigrams: LogPairedDoubleCounter[T,T]): Automaton[Double,Option[T],T] = {
+
+    val marginal = marginalizeCounts(bigrams);
+    val selectedStates = selectStates(maxStates,marginal,bigrams);
+    //println(selectedStates);
+    for( a <- selectedStates.keysIterator) {
+      gramIndex.index(a);
+    }
+
+    // a few useful states
+    val unigramState = gramIndex(None);
+    /*
+    val startState = {
+      if(selectedStates contains Seq(beginningUnigram))
+        gramIndex(Seq(beginningUnigram))
+      else if(selectedStates contains Seq(beginningUnigram,beginningUnigram))
+        gramIndex(Seq(beginningUnigram,beginningUnigram))
+      else unigramState;
+    }
+    */
+    
+    val startState = {
+      if(selectedStates contains Some(beginningUnigram))
+        (Some(beginningUnigram))
+      else None
+    }
+
+    val endingChars = beginningUnigram;
+
+    val divArcs = for {
+      (chars,state) <- selectedStates.iterator;
+      arc <- arcsForCounter(chars,state.transitions)
+    } yield arc;
+    //  idx1 = gramIndex(chars);
+   
+    val finalWeights = for {
+      (chars, state) <- selectedStates
+      //idx1 = gramIndex(chars);
+      finalScore = finalWeight(chars, state.transitions);
+      if finalScore != Double.NegativeInfinity
+    } yield (chars,finalScore - state.transitions.logTotal);
+
+    val auto = Automaton.automaton(Map(startState->prob),Map.empty ++ finalWeights)(
+      (divArcs).toSeq :_*
+    );
+    auto
+  }
 }
 
 import scalanlp.fst.Arc;
