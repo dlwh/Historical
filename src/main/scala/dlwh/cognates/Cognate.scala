e7103998053f6a7cd7b8b173e6f9fe23ad9d232e
David Hall <dlwh@berkeley.edu>
2010-02-10 01:05:18 -0800

    Add a way to prune datasets, a pruned version of ipa.

--- src/main/scala/dlwh/cognates/Cognate.scala
+++ src/main/scala/dlwh/cognates/Cognate.scala
@@ -1,5 +1,6 @@
 package dlwh.cognates;
 import scala.io._;
+import java.io._;
 
 case class Cognate(word: String, language: String);
 
@@ -16,12 +17,27 @@ object Cognates {
     val stream = this.getClass.getClassLoader().getResourceAsStream(file);
     val src = Source.fromInputStream(stream)(Codec.UTF8).getLines().filter(!_.startsWith("#"));
     val cognates = (for(line <- src) yield {
-      for( ((w,c),l) <- line.split(' ').zipWithIndex.toSeq zip languages) 
+      for( ((w,c),l) <- line.split(' ').zipWithIndex.toSeq zip languages if w != "?")
         yield Cognate(w,l);
     }).toSeq;
 
     stream.close();
     cognates.toSeq;
+  }
+}
+
+object PruneCognates {
+  def main(args: Array[String]) {
+    val percentage = args(1).toDouble;
+    val src = Source.fromFile(new File(args(0)))(Codec.UTF8).getLines().filter(!_.startsWith("#"));
+    for(line <- src) {
+      val words = for( w <- line.split(' ') ) yield {
+        if(Math.random < percentage) "?"
+        else w;
+      }
+      println(words.mkString(" "));
+    }
+
   }
 }
 
