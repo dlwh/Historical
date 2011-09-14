69c9c309c99c29cca2e5ec3b8867ada6fe7d2ff3
David Hall <dlwh@berkeley.edu>
2011-09-13 20:22:49 -0700

    parsim now works

--- src/main/scala/dlwh/parsim/Parsim.scala
+++ src/main/scala/dlwh/parsim/Parsim.scala
@@ -29,6 +29,7 @@ object Parsim extends App {
   val leaves = baseTree.leaves.toSet
   val cognates = dataset.cognates.flatten.filter(c => leaves(c.language));
   val groupedCognates = groupCognates(cognates)
+  val gold = new CognatesEval(dataset.cognates.map(_.filter(c => leaves(c.language))));
 
   val ntaxa = langIndex.size
 
@@ -40,15 +41,17 @@ object Parsim extends App {
 
   type T = Set[Language]
 
-  def initWeight(x: Int) = -3.
-  def transRatio(x: Int, b: Int) = if(x == b) 0 else -4
-  val ed = new GeneralEditDistance(1, charIndex, initWeight, initWeight, transRatio)
+  def initWeight(x: Int) = -4. + x
+  def subWeight(x: Int) = -4. - x
+  def transRatio(x: Int, b: Int) = if(x == b) 0.0 else -1.
+  val ed = new GeneralEditDistance(1, charIndex, subWeight, initWeight, transRatio)
   val factorsFactory = new InnovationFactorsFactory(new WordFactorsFactory(ed),
-                                                    new LanguageModelFactorsFactory(charIndex), 0.2)
+                                                    new LanguageModelFactorsFactory(charIndex), 0.4)
 
   import factorsFactory.Factors
 
 
+  var decoded:IndexedSeq[IndexedSeq[Cognate]] = null
   def likelihood(initParams: Map[T,factorsFactory.EdgeParameters], annTree: Tree[T]):Double = {
     def cognateFor(group: Map[Language,IndexedSeq[Cognate]], lang: Set[Language]) = {
       if(lang.size == 1) group.get(lang.iterator.next).map(_.head)
@@ -65,8 +68,14 @@ object Parsim extends App {
         new TreeInference(factors, annTree, { (x:T) => cognateFor(c, x)})
       }
       val newLikelihood : Double = inference.par.map(_.likelihood).sum
-      println(newLikelihood)
-      if( (newLikelihood - likelihood)/newLikelihood.abs < 1E-4 && iter > 3) return newLikelihood
+      val change = (newLikelihood - likelihood) / newLikelihood.abs
+      decoded = inference.par.flatMap(decodeCognates(_,annTree)).seq.toIndexedSeq
+
+      val (p,r)  = gold.precisionAndRecall(decoded.map(g => CognateGroup(g:_*)))
+      val f1 = 2 * p * r / (p + r)
+
+      println(newLikelihood,likelihood,change,p,r,f1)
+      if( change < 1E-4 && iter > 3) return newLikelihood
 
       likelihood = newLikelihood
       val ecounts = inference.par.map(_.sufficientStatistics).reduceLeft(sumStats _)
@@ -99,5 +108,30 @@ object Parsim extends App {
     }
     result.toMap
 
+  }
+
+  def decodeCognates(inference: TreeInference[T,factorsFactory.Factors[T]],
+                     tree: Tree[T],
+                     cur: ArrayBuffer[Cognate]= new ArrayBuffer[Cognate]):IndexedSeq[IndexedSeq[Cognate]] = {
+    val label = tree.label
+    val children = tree.children
+    val childGroups = for(c <- children if inference.hasUpwardMessage(c.label)) yield {
+      val edges = inference.edgeMarginal(c.label);
+      val fromChild = if(c.isLeaf) {
+        var rest = IndexedSeq.empty[Cognate];
+        for( (w,e) <- inference.cognates(c.label) zip edges) {
+          if(e.posteriorInnovationProb >= 0.5) rest :+= w
+          else cur += w
+        }
+        rest.map(IndexedSeq(_));
+      } else if(edges.head.posteriorInnovationProb < 0.5){
+        decodeCognates(inference,c,cur).drop(1)
+      } else {
+        decodeCognates(inference,c);
+      }
+      fromChild;
+    }
+
+    (cur +: (childGroups.flatten.filterNot(_.isEmpty))).toIndexedSeq;
   }
 }
